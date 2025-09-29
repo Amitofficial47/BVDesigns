f82dcdb17c318853989964597a2f22d29e0733fa
AMIT KUMAR <amitmitu2235@gmail.com>
2025-09-29 13:49:42 +0530

    Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,42 +1,53 @@
-import type {Metadata} from 'next';
-import './globals.css';
-import { Header } from '@/components/header';
-import { Footer } from '@/components/footer';
-import { Toaster } from "@/components/ui/toaster"
-import { ThemeProvider } from "@/components/theme-provider"
+import type { Metadata } from "next";
+import "./globals.css";
+import { Header } from "@/components/header";
+import { Footer } from "@/components/footer";
+import { Toaster } from "@/components/ui/toaster";
+import { ThemeProvider } from "@/components/theme-provider";
+import { Analytics } from "@vercel/analytics/next";
 
 export const metadata: Metadata = {
-  title: 'B&V Designs',
-  description: 'An architectural studio crafting spaces that inspire and endure.',
+	title: "B&V Designs",
+	description:
+		"An architectural studio crafting spaces that inspire and endure.",
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en" className="scroll-smooth" suppressHydrationWarning>
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@400;700&family=Roboto:wght@400;500;700&display=swap" rel="stylesheet" />
-      </head>
-      <body className="font-body antialiased">
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="light"
-          enableSystem
-          disableTransitionOnChange
-        >
-            <div className="flex flex-col min-h-screen">
-              <Header />
-              <main className="flex-grow">{children}</main>
-              <Footer />
-            </div>
-            <Toaster />
-        </ThemeProvider>
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en" className="scroll-smooth" suppressHydrationWarning>
+			<head>
+				<link rel="preconnect" href="https://fonts.googleapis.com" />
+				<link
+					rel="preconnect"
+					href="https://fonts.gstatic.com"
+					crossOrigin="anonymous"
+				/>
+				<link
+					href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@400;700&family=Roboto:wght@400;500;700&display=swap"
+					rel="stylesheet"
+				/>
+			</head>
+			<body className="font-body antialiased">
+				<ThemeProvider
+					attribute="class"
+					defaultTheme="light"
+					enableSystem
+					disableTransitionOnChange
+				>
+					<div className="flex flex-col min-h-screen">
+						<Header />
+						<main className="flex-grow">
+							{children} <Analytics />
+						</main>
+						<Footer />
+					</div>
+					<Toaster />
+				</ThemeProvider>
+			</body>
+		</html>
+	);
 }
